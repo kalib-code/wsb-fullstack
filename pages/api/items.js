09e2db83ab75bae2a2f06a38e4f6cd7fd4c04945
Karl Kenneth Alibuas <kalibuas@gmail.com>
2022-07-09 12:36:38 +0800

    dynamoDB

--- pages/api/items.js
+++ pages/api/items.js
@@ -1,9 +1,8 @@
-import * as uuid from 'uuid';
-import dynamoDb from '../../lib/dynamodb';
+import * as uuid from "uuid";
+import dynamoDb from "../../lib/dynamodb";
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    
+  if (req.method === "POST") {
     const item = {
       id: uuid.v4(),
       ...req.body,
@@ -11,42 +10,50 @@ export default async function handler(req, res) {
     };
 
     await dynamoDb.put({
+      TableName: "WSB_Dev_Users",
       Item: item,
     });
 
     res.status(201).json(item);
   }
 
-  if (req.method === 'GET') {
-    const { Item } = await dynamoDb.get({
-      Key: {
-        id: req.query.id
-      }
-    });
+  if (req.method === "GET") {
+    if (req.query.id) {
+      const { Item } = await dynamoDb.get({
+        Key: {
+          id: req.query.id,
+        },
+      });
 
-    res.status(200).json(Item);
+      res.status(200).json(Item);
+    }
+
+    const { Items } = await dynamoDb.query({
+      TableName: process.env.TABLE_NAME,
+    });
+    res.status(200).json(Items);
   }
 
-  if (req.method === 'PUT') {
+  if (req.method === "PUT") {
     const { Attributes } = await dynamoDb.update({
       Key: {
-        id: req.body.id
+        id: req.body.id,
       },
-      UpdateExpression: 'SET content = :content',
+      UpdateExpression: "SET content = :content",
       ExpressionAttributeValues: {
-        ':content': req.body.content || null
+        ":content": req.body.content || null,
       },
-      ReturnValues: 'ALL_NEW'
+      ReturnValues: "ALL_NEW",
     });
 
     res.status(200).json(Attributes);
   }
 
-  if (req.method === 'DELETE') {
+  if (req.method === "DELETE") {
     await dynamoDb.delete({
       Key: {
-        id: req.query.id
-      }
+        id: req.query.id,
+      },
     });
 
     res.status(204).json({});
